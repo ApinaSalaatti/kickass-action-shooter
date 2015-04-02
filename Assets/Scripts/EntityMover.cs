97474750e2637a351b480b31fb6a8dc1e2c2cf84
Merioksan Mikko <=>
2015-04-02 12:06:00 +0300

    run and shoot boom boom boom

---
+++ Assets/Scripts/EntityMover.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class EntityMover : MonoBehaviour {
+	public float speed = 5f;
+
+	public Vector2 Movement {
+		get; set;
+	}
+
+	private Rigidbody2D rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody.velocity = Movement * speed;
+	}
+}
---
+++ Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponManager : MonoBehaviour {
+	public float fireInterval = 0.2f;
+	public GameObject bulletPrefab;
+
+	public bool Firing {
+		get; set;
+	}
+	public Vector2 AimTowards {
+		get; set;
+	}
+
+	private float fireTimer;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		fireTimer += Time.deltaTime;
+		if(Firing && fireTimer >= fireInterval) {
+			fireTimer = 0f;
+			GameObject b = Instantiate(bulletPrefab, transform.position, Quaternion.identity) as GameObject;
+			b.GetComponent<EntityMover>().Movement = AimTowards;
+		}
+	}
+}
